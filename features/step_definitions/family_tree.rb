398fef2e9f42495d516b5cd75d6b2c127de4e630
holler4amarshall <holler4amarshall@hotmail.com>
2016-11-07 18:43:36 +1000

    add basic step definitions auto generated by cucumber cmd

---
+++ features/step_definitions/family_tree.rb
@@ -0,0 +1,48 @@
+require 'selenium-webdriver'
+require 'page-object'
+require_relative ''
+
+
+Given(/^I open the family tree questionnaire$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I see the welcome message$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I see the "([^"]*)" question$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I answer "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I am redirected to the "([^"]*)" screen$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the "([^"]*)" response$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the "([^"]*)" question$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I answered No to orphan question$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I answered "([^"]*)" to parents alive and married question$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I answered "([^"]*)" to the divorced or widowed question$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I am redirected to the "([^"]*)" response$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
